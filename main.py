eee69c0d93d9327a824577ee8413171955580b96
gcvilon <gone58799@gmail.com>
2025-02-27 18:25:55 +0300

    correcton error

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from ctypes import c_int
-
 import psycopg2
-from psycopg2 import connect
+
 
 
 class DataBase:
@@ -11,127 +9,144 @@ class DataBase:
         self.user = 'postgres'
         self.password = 'lich5800'
 
-    def db_con(self):
-        connection = psycopg2.connect(database=self.database, user=self.user, password=self.password)
-        return connection
-
     def create_tables(self):
-        connection = self.db_con()
-        with connection.cursor() as cur:
-            cur.execute('''
-            CREATE TABLE IF NOT EXISTS clients (
-                client_id   SERIAL      PRIMARY KEY,
-                first_name  VARCHAR(20) NOT NULL,
-                last_name   VARCHAR(20) NOT NULL,
-                email       VARCHAR(30) NOT NULL
-            );
-            ''')
-            cur.execute('''
-            CREATE TABLE IF NOT EXISTS phone_numbers (
-                phone_id    SERIAL      PRIMARY KEY,
-                client_id     INT         REFERENCES clients(client_id),
-                number      VARCHAR(20) NOT NULL
-            );
-            ''')
-            connection.commit()
-        connection.close()
+        with psycopg2.connect(database=self.database, user=self.user, password=self.password) as connection:
+            with connection.cursor() as cur:
+                cur.execute('''
+                CREATE TABLE IF NOT EXISTS clients (
+                    client_id   SERIAL      PRIMARY KEY,
+                    first_name  VARCHAR(20) NOT NULL,
+                    last_name   VARCHAR(20) NOT NULL,
+                    email       VARCHAR(30) NOT NULL
+                );
+                ''')
+                cur.execute('''
+                CREATE TABLE IF NOT EXISTS phone_numbers (
+                    phone_id    SERIAL      PRIMARY KEY,
+                    client_id     INT         REFERENCES clients(client_id),
+                    number      VARCHAR(20) NOT NULL
+                );
+                ''')
         print('Таблицы созданы')
 
     def write_client(self, first_name, last_name, email):
-        connection = self.db_con()
-        with connection.cursor() as cur:
-            cur.execute('''
-            INSERT INTO clients(first_name, last_name, email)
-            VALUES(%s, %s, %s)
-            ''', (first_name, last_name, email))
-            connection.commit()
-        connection.close()
+        with psycopg2.connect(database=self.database, user=self.user, password=self.password) as connection:
+            with connection.cursor() as cur:
+                cur.execute('''
+                INSERT INTO clients(first_name, last_name, email)
+                VALUES(%s, %s, %s)
+                ''', (first_name, last_name, email))
         print('Клиент записан')
 
     def write_number(self, client_id, phone_number):
-        connection = self.db_con()
-        with connection.cursor() as cur:
-            cur.execute('''
-            INSERT INTO phone_numbers(client_id, number)
-            VALUES(%s, %s)
-            ''', (client_id, phone_number))
-            connection.commit()
-        connection.close()
+        with psycopg2.connect(database=self.database, user=self.user, password=self.password) as connection:
+            with connection.cursor() as cur:
+                cur.execute('''
+                INSERT INTO phone_numbers(client_id, number)
+                VALUES(%s, %s)
+                ''', (client_id, phone_number))
         print('Номер телефона записан')
 
-    def update_info(self, client_id, first_name, last_name, email):
-        connection = self.db_con()
-        with connection.cursor() as cur:
-            cur.execute('''
-            UPDATE clients 
-            SET first_name = %s, last_name = %s, email = %s
-            WHERE client_id = %s;
-            ''', (first_name, last_name, email, client_id))
-            connection.commit()
-        connection.close()
+    def update_info(self, client_id=None, first_name=None, last_name=None, email=None):
+        if client_id is None:
+            print("Ошибка: Необходимо указать client_id.")
+            return
+        with psycopg2.connect(database=self.database, user=self.user, password=self.password) as connection:
+            with connection.cursor() as cur:
+                updates = []
+                params = []
+                if first_name is not None:
+                    updates.append("first_name = %s")
+                    params.append(first_name)
+                if last_name is not None:
+                    updates.append("last_name = %s")
+                    params.append(last_name)
+                if email is not None:
+                    updates.append("email = %s")
+                    params.append(email)
+                if not updates:
+                    print("Ошибка: Не указаны данные для обновления.")
+                    return
+                query = f'''
+                UPDATE clients 
+                SET {', '.join(updates)}
+                WHERE client_id = %s;
+                '''
+                params.append(client_id)
+                cur.execute(query, params)
         print('Данные клиента обновлены')
 
     def delete_number(self, client_id, number):
-        connection = self.db_con()
-        with connection.cursor() as cur:
-            cur.execute('''
-            DELETE FROM phone_numbers
-            WHERE client_id = %s 
-            AND number = %s;
-            ''', (client_id, number))
-            connection.commit()
-        connection.close()
+        with psycopg2.connect(database=self.database, user=self.user, password=self.password) as connection:
+            with connection.cursor() as cur:
+                cur.execute('''
+                DELETE FROM phone_numbers
+                WHERE client_id = %s 
+                AND number = %s;
+                ''', (client_id, number))
         print('Номер телефона удален')
 
     def delete_client(self, client_id):
-        connection = self.db_con()
-        with connection.cursor() as cur:
-            cur.execute('''
-            DELETE FROM phone_numbers
-            WHERE client_id = %s;
-            
-            DELETE FROM clients
-            WHERE client_id = %s;
-            ''', (client_id, client_id))
-            connection.commit()
-        connection.close()
+        with psycopg2.connect(database=self.database, user=self.user, password=self.password) as connection:
+            with connection.cursor() as cur:
+                cur.execute('''
+                DELETE FROM phone_numbers
+                WHERE client_id = %s;
+                
+                DELETE FROM clients
+                WHERE client_id = %s;
+                ''', (client_id, client_id))
         print('Клиент удален')
 
-    def find_client(self, first_name, last_name, email, phone_number):
-        connection = self.db_con()
-        with connection.cursor() as cur:
-            client = cur.execute('''
-            SELECT clients.client_id, clients.first_name, clients.last_name, phone_numbers.number
-            FROM clients, phone_numbers
-            WHERE clients.first_name = %s
-            AND clients.last_name = %s
-            AND clients.email = %s
-            AND clients.client_id = phone_numbers.client_id
-            AND phone_numbers.number = %s;
-            ''', (first_name, last_name, email, phone_number))
-            client = cur.fetchall()
-        connection.close()
-        if client == []:
+    def find_client(self, first_name=None, last_name=None, email=None, phone_number=None):
+        with psycopg2.connect(database=self.database, user=self.user, password=self.password) as connection:
+            with connection.cursor() as cur:
+                query = '''
+                SELECT clients.client_id, clients.first_name, clients.last_name, clients.email, phone_numbers.number
+                FROM clients
+                LEFT JOIN phone_numbers ON clients.client_id = phone_numbers.client_id
+                WHERE (%s IS NULL OR clients.first_name = %s)
+                  AND (%s IS NULL OR clients.last_name = %s)
+                  AND (%s IS NULL OR clients.email = %s)
+                  AND (%s IS NULL OR phone_numbers.number = %s);
+                '''
+                cur.execute(query,
+                            (first_name, first_name, last_name, last_name, email, email, phone_number, phone_number))
+                client_data = cur.fetchall()
+
+        if not client_data:
             print('Клиент не найден')
         else:
-            print(f'''Клиент найден:
-            ID: {client[0][0]}
-            Имя: {client[0][1]}
-            Фамилия: {client[0][2]}
-            Email: {client[0][3]}
-            Номера телефонов: {", ".join(set(item[3] for item in client))}
-            ''')
+            clients = {}
+            for row in client_data:
+                client_id, first_name, last_name, email, phone_number = row
+                if client_id not in clients:
+                    clients[client_id] = {
+                        'first_name': first_name,
+                        'last_name': last_name,
+                        'email': email,
+                        'phone_numbers': set()
+                    }
+                if phone_number:
+                    clients[client_id]['phone_numbers'].add(phone_number)
+            for client_id, data in clients.items():
+                print(f'''Клиент найден:
+                        ID: {client_id}
+                        Имя: {data['first_name']}
+                        Фамилия: {data['last_name']}
+                        Email: {data['email']}
+                        Номера телефонов: {", ".join(data['phone_numbers']) if data['phone_numbers'] else "Нет данных"}
+                        ''')
 
 def main():
     db = DataBase()
     db.create_tables()
     db.write_client('John', 'Emin', 'home@ll.ry')
     db.write_number(1, '+79538954687')
-    db.write_number(1, '895565544')
-    db.update_info(1,'Hom', 'Jim', 'hhукеh@mmm.hj')
+    db.update_info(client_id=1,first_name='Home', last_name='Jim', email='hhукеh@mmm.hj')
     db.delete_number(1, '+79538954687')
     db.delete_client(1)
-    db.find_client('Hom', 'Jim', 'hhукеh@mmm.hj', '895565544')
+    db.find_client(last_name='Emin')
 
 if __name__ == '__main__':
     main()
